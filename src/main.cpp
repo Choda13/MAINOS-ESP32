6da329253e19c707fa4e57c6fd02a9699ca2f25c
choda13 <kovacevic.danilo2003@gmail.com>
2021-09-27 23:57:00 +0200

    Removed test code from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,50 +9,16 @@
 const char *ssid = "ZTE_H168N87DF8C"; // Enter SSID here
 const char *password = "mojnet021";   //Enter Password here
 
-WebServer server(80);
 LedStrip strip(5, 60);
-
-void onPost();
-void WebServerInit();
 
 void setup()
 {
     Serial.begin(115200);
     strip.show();
     mainos_init();
-    WebServerInit();
 }
 
 void loop()
 {
-    server.handleClient();
     delay(10);
-}
-
-void onPost(){
-    server.arg("plain");
-
-}
-
-void WebServerInit(){
-    Serial.println("Connecting to ");
-    Serial.println(ssid);
-
-    //connect to your local wi-fi network
-    WiFi.begin(ssid, password);
-
-    //check wi-fi is connected to wi-fi network
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(1000);
-        Serial.print(".");
-    }
-    Serial.println("");
-    Serial.println("WiFi connected..!");
-    Serial.print("Got IP: ");
-    Serial.println(WiFi.localIP());
-
-    server.on("/post", HTTP_POST, onPost);
-    server.begin();
-    Serial.println("HTTP server started");
 }
