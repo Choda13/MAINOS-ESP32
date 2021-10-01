632914dc1486fdecf07ff71293753ee15ee923a1
choda13 <kovacevic.danilo2003@gmail.com>
2021-10-01 16:01:13 +0200

    type_to_hex method changed

--- include/MAINOS.h
+++ include/MAINOS.h
@@ -2,6 +2,7 @@
 #include "API/API.h"
 #include "StatusCodes.h"
 #include "CommandHandler/CommandHandler.h"
+#include "TypeConversions/TypeConversions.h"
 
 int mainos_init();
 static commserv::CommandHandler CommandService;
--- include/TypeConversions/TypeConversions.h
+++ include/TypeConversions/TypeConversions.h
@@ -5,33 +5,50 @@
 #include <iomanip>
 #include <iostream>
 
-namespace TypeConversions 
+namespace TypeConversions
 {
-	static std::string int_to_string(int value) 
+	static std::string int_to_string(int value);
+	template <typename T>
+	static std::string byte_to_hex(T val);
+	template <typename T>
+	static std::vector<uint8_t> type_to_bytes(T value);
+	template <typename T>
+	static T bytes_to_type(std::vector<uint8_t> bytes, bool ignoreSize = true);
+
+	static std::string int_to_string(int value)
 	{
 		std::stringstream s;
 		s << value;
 		return s.str();
 	}
-	template<typename T>static std::string byte_to_hex(T i)
+	template <typename T>
+	static std::string byte_to_hex(T val)
 	{
-		std::stringstream stream;
-		stream << std::setfill('0') << std::setw(sizeof(T) * 2)
-			<< std::hex << (T)i;
-		return stream.str();
-	}
-	template<typename T>static std::vector<uint8_t> type_to_bytes(T value)
-	{
-		std::vector<uint8_t> result(sizeof(T));
-		std::copy(reinterpret_cast<uint8_t*>(&value), reinterpret_cast<uint8_t*>(&value) + sizeof(T), result.data());
+		std::string result = "";
+		std::vector<uint8_t> bytes = type_to_bytes<T>(val);
+		char const hex_chars[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+		for (auto it = bytes.begin(); it != bytes.end(); ++it)
+		{
+			char const byte = *it;
+			result += hex_chars[(byte & 0xF0) >> 4];
+			result += hex_chars[(byte & 0x0F) >> 0];
+		}
 		return result;
 	}
-	template<typename T>static T bytes_to_type(std::vector<uint8_t>bytes, bool ignoreSize = true)
+	template <typename T>
+	static std::vector<uint8_t> type_to_bytes(T value)
+	{
+		std::vector<uint8_t> result(sizeof(T));
+		std::copy(reinterpret_cast<uint8_t *>(&value), reinterpret_cast<uint8_t *>(&value) + sizeof(T), result.data());
+		return result;
+	}
+	template <typename T>
+	static T bytes_to_type(std::vector<uint8_t> bytes, bool ignoreSize)
 	{
 		T value;
-		std::copy(bytes.data(), bytes.data() + sizeof(T), reinterpret_cast<uint8_t*>(&value));
+		std::copy(bytes.data(), bytes.data() + sizeof(T), reinterpret_cast<uint8_t *>(&value));
 		//if (!ignoreSize && sizeof(T) != bytes.size())
-			//Serial print "Exception at bytes_to_type(std::vector<uint8_t>bytes): bytes size doesnt equal the size of type T"
+		//Serial print "Exception at bytes_to_type(std::vector<uint8_t>bytes): bytes size doesnt equal the size of type T"
 		return value;
 	}
 }
