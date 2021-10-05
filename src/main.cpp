877dc34080b0bf40e0d7240dd885a16fffb6e0ec
choda13 <kovacevic.danilo2003@gmail.com>
2021-10-05 20:49:45 +0200

    JSON and url parser implemented,only url completed

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
-#include <Arduino.h>
-#include <iostream>
 #include <WiFi.h>
+#include <vector>
+#include <iostream>
+#include <Arduino.h>
 #include <WebServer.h>
 #include "MAINOS.h"
 #include "LedAPI/LedAPI.h"
 #include "LedLib/LedStrip.h"
+#include "ArduinoJson.h"
 
 WebServer server(80);
 
@@ -12,6 +14,10 @@ const char *SSID = "ZTE_H168N87DF8C";
 const char *PASS = "mojnet021";
 
 void post_page();
+
+std::vector<Command> ParseUrlEncoded();
+std::vector<Command> ParseJSONEncoded();
+
 uint8_t connectToWifi(const char *ssid, const char *pass);
 
 int char2int(char input);
@@ -61,31 +67,23 @@ uint8_t connectToWifi(const char *ssid, const char *pass)
 
 void post_page()
 {
-    if(!server.hasArg("API_ID") ||
-       !server.hasArg("COMM_ID") ||
-       !server.hasArg("ARGS_SIZE") ||
-       !server.hasArg("args")){
-        server.send(400, "text/plain", "Moras poslati sve parametre idiote");
-        return;
-       }
-    String hexData="";
-    hexData+=server.arg("API_ID");
-    hexData+=server.arg("COMM_ID");
-    hexData+=server.arg("ARGS_SIZE");
-    hexData+=server.arg("args");
+    std::string contentType;
+    std::vector<Command> commands;
 
-    if(hexData.length() % 2 || !validateHex(hexData.c_str()))
-        server.send(400, "text/plain", "Vrednost parametara mora biti u hex formati majmune glupi");
+    contentType = server.header("Content-Type").c_str();
+    if(contentType == "application/json")
+        commands = commands = ParseJSONEncoded();
+    else if(contentType == "application/x-www-form-urlencoded")
+        commands = ParseUrlEncoded();
+    else
+        commands = ParseUrlEncoded();
 
-    std::vector<uint8_t> bytes(hexData.length()/2);
-    hex2bin(hexData.c_str(), bytes.data());
+    auto result = CommandService.HandleCommands(commands);
 
-    auto result = CommandService.HandleCommands(bytes);
+    std::string response = "";
+    for (auto &&i : result)
+        response += i.toString();
 
-    std::string response="";
-    for(auto &&i : result)
-        response+= i.toString();
-    
     server.send(200, "text/plain", response.c_str());
 }
 
@@ -111,10 +109,72 @@ void hex2bin(const char *src, uint8_t *target)
 
 bool validateHex(const char *data)
 {
-    while(*data){
-        if(char2int(data[0]) == -1)
+    while (*data)
+    {
+        if (char2int(data[0]) == -1)
             return false;
         data++;
     }
     return true;
+}
+
+std::vector<Command> ParseUrlEncoded()
+{
+    uint8_t API_ID;
+    uint8_t COMM_ID;
+    unsigned int arg_size;
+    String args;
+    std::vector<Command> commands;
+
+    if (!server.hasArg("API_ID") || !server.hasArg("COMM_ID"))
+        server.send(400, "text/plain", "Moras poslati bar dva parametra, API_ID i COMM_ID");
+
+    API_ID = server.arg("API_ID").toInt();
+    COMM_ID = server.arg("COMM_ID").toInt();
+    arg_size = server.arg("ARGS_SIZE").toInt();
+
+    if (arg_size != server.arg("ARGS").length())
+    {
+        Serial.println("ParseUrlEncoded: Velicina argumenta ne odgovara datoj velicini argumenata.");
+        server.send(400, "ParseUrlEncoded: Velicina argumenta ne odgovara datoj velicini argumenata.");
+        return commands;
+    }
+
+    args = server.arg("ARGS");
+
+    if (args.length() % 2 || !validateHex(args.c_str()))
+        server.send(400, "text/plain", "Vrednost parametara mora biti u hex formati majmune glupi");
+
+    //Convert arguments from hex format to its repsective value
+    std::vector<uint8_t> bytes(arg_size);
+    hex2bin(args.c_str(), bytes.data());
+
+    commands.push_back(Command(API_ID, COMM_ID, bytes));
+    return commands;
+}
+std::vector<Command> ParseJSONEncoded()
+{
+    StaticJsonDocument<4096> doc;
+    std::vector<Command> commands;
+    String json = server.arg("plain");
+
+    if (json.length() > 4096)
+    {
+        server.send(400, "Preveliki JSON, salji JSON dokument koji nije veci od 4096 bajtova");
+        return commands;
+    }
+
+    auto error = deserializeJson(doc, json);
+    // Test if parsing succeeds.
+    if (error)
+    {
+        std::string msg = "deserializeJson() failed: ";
+        msg+=error.c_str();
+        Serial.println(msg.c_str());
+        server.send(500, msg.c_str());
+        return commands;
+    }
+
+    return commands;
+
 }
